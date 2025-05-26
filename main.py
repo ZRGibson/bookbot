cf66051cde3aafd953ddc4b6ea6c960c3f73790e
ZRGibson <zachary.ryan.gibson@gmail.com>
2025-05-26 03:01:41 -0500

    final touches and sys.argv additon

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_num_words, get_num_chars, get_ordered_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     num_words = get_num_words(book_path)
     num_chars = get_num_chars(book_path)
     ordered_list = get_ordered_list(num_chars)
